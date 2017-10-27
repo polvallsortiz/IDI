460d506b0db5b877ed1627be8326befd231498ac
polvallsortiz <pol.valls.ortiz@est.fib.upc.edu>
2017-10-27 12:09:58 +0200

    BLOC_2 EXERCICI_3 final exercise

--- BLOC_2/SESSIO_1/EXERCICI_1/ui_MyForm.h
+++ BLOC_2/SESSIO_1/EXERCICI_1/ui_MyForm.h
@@ -1,7 +1,7 @@
 /********************************************************************************
 ** Form generated from reading UI file 'MyForm.ui'
 **
-** Created by: Qt User Interface Compiler version 5.5.1
+** Created by: Qt User Interface Compiler version 5.9.1
 **
 ** WARNING! All changes made in this file will be lost when recompiling UI file!
 ********************************************************************************/
@@ -77,8 +77,8 @@ public:
 
     void retranslateUi(QWidget *MyForm)
     {
-        MyForm->setWindowTitle(QApplication::translate("MyForm", "Form", 0));
-        pushButton->setText(QApplication::translate("MyForm", "&Sortir", 0));
+        MyForm->setWindowTitle(QApplication::translate("MyForm", "Form", Q_NULLPTR));
+        pushButton->setText(QApplication::translate("MyForm", "&Sortir", Q_NULLPTR));
     } // retranslateUi
 
 };
